6a07d66415edc1e3d439138075f0ce16f1d985d2
Andreas Blaette <andreas.blaette@uni-due.de>
2020-04-01 17:16:19 +0200

    download from zenodo

--- R/download.R
+++ R/download.R
@@ -1,17 +1,24 @@
-#' Download GermaParl from Web Directory.
+#' Download GermaParl from zenodo.
 #' 
 #' \code{germaparl_download_corpus} will get a tarball with the indexed corpus
 #' from a designated web space or a local directory and install the corpus into
 #' the GermaParl package.
-#' @param tarball Name of the tarball.
-#' @param dir directory where the tarball resides, either the URL of a webspace,
-#'   or a local directory
+#' @param doi The DOI (Digital Object Identifier) of the GermaParl tarball at zenodo.
+#' @param quiet Whether to output messages.
 #' @export germaparl_download_corpus
 #' @rdname installation
 #' @importFrom cwbtools corpus_install
-#' @importFrom RCurl url.exists
-germaparl_download_corpus <- function(tarball = "germaparl.tar.gz", dir = "http://polmine.sowi.uni-due.de/corpora/cwb/germaparl"){
-  tarball <- file.path(dir, tarball)
-  message("... downloading tarball: ", tarball)
+#' @importFrom RCurl url.exists getURL
+#' @importFrom jsonlite fromJSON
+germaparl_download_corpus <- function(doi = "https://doi.org/10.5281/zenodo.3735141", quiet = FALSE){
+  if (isFALSE(is.logical(quiet))) stop("Argument 'quiet' needs to be a logical value.")
+  if (isFALSE(grepl("^.*?10\\.5281/zenodo\\.\\d+$", doi))){
+    stop("Argument 'doi' is expected to offer a DOI (Digital Object Identifier) that refers to data",
+         "hosted with zenodo, i.e. starting with 10.5281/zenodo.")
+  }
+  record_id <- gsub("^.*?10\\.5281/zenodo\\.(\\d+)$", "\\1", doi)
+  zenodo_api_url <- sprintf("https://zenodo.org/api/records/%d", as.integer(record_id))
+  tarball <- fromJSON(getURL(zenodo_api_url))[["files"]][["links"]][["self"]]
+  if (isFALSE(quiet)) message("... downloading tarball: ", tarball)
   corpus_install(pkg = "GermaParl", tarball = tarball)
 }
