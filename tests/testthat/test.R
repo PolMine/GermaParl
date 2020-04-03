795766ed647f2ed16f0d45acb6c290be9b49d012
Andreas Blaette <andreas.blaette@uni-due.de>
2020-04-03 09:46:30 +0200

    tests reorganized

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,16 +1,29 @@
 testthat::context("download")
 
 test_that(
-  "tests",
+  "corpus not available initially",
   {
     expect_identical(germaparl_is_installed(), FALSE)
+  }
+)
+
+
+germaparl_download_corpus()
+
+test_that(
+  "availability of corpus after installation",
+  {
     skip_on_cran()
-    
-    germaparl_download_corpus()
     expect_identical(germaparl_is_installed(), TRUE)
     expect_identical(germaparl_get_doi(), "https://doi.org/10.5281/zenodo.3735141")
     expect_identical(germaparl_get_version(), "v1.0.5")
-    
+  }
+)
+
+
+test_that(
+  "add s-attribute speech",
+  {
     library(polmineR)
     use("GermaParl")
     germaparl_add_s_attribute_speech()
