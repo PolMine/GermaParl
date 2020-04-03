0c8bdb244393b912157f8b67a99e19eb92e6e286
Andreas Blaette <andreas.blaette@uni-due.de>
2020-04-03 09:32:50 +0200

    check works

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,7 +1,7 @@
 testthat::context("download")
 
 test_that(
-  "initial test",
+  "tests",
   {
     expect_identical(germaparl_is_installed(), FALSE)
     skip_on_cran()
@@ -11,27 +11,9 @@ test_that(
     expect_identical(germaparl_get_doi(), "https://doi.org/10.5281/zenodo.3735141")
     expect_identical(germaparl_get_version(), "v1.0.5")
     
-    germaparl_add_s_attribute_speech()
     library(polmineR)
     use("GermaParl")
-    
-    s_attrs <- s_attributes("GERMAPARL")
-    expect_identical(TRUE, "speech" %in% s_attrs)
-    
-    s_attrs <- s_attributes("GERMAPARL", "speech")
-    expect_identical(length(s_attrs), 162023L)
-    
-  }
-)
-
-test_that(
-  "germaparl_add_s_attribute_speech",
-  {
-    expect_identical(germaparl_is_installed(), TRUE)
-    skip_on_cran()
-    
     germaparl_add_s_attribute_speech()
-    library(polmineR)
     use("GermaParl")
     
     s_attrs <- s_attributes("GERMAPARL")
